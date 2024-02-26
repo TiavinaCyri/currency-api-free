b8dbc43d896b340af5770d9226f0b6bb10f69f9e
tiavinacyri <tiavinacyri@gmail.com>
2024-02-26 15:45:05 +0400

    init

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/:currencyCode/:toCurrencyCode', async (req, res) => {
+    const currencyCode = req.params.currencyCode.toUpperCase();
+    const toCurrencyCode = req.params.toCurrencyCode.toUpperCase();
+
+    try {
+        const apiUrl = `https://api.exchangeit.app/currencies/${currencyCode}/latest`;
+        const apiResponse = await fetch(apiUrl);
+
+        if (apiResponse.ok) {
+            const result = await apiResponse.json();
+            const data = result.data.rates
+            const element = data.find((element) => element.code === toCurrencyCode);
+            if (!element) {
+                res.status(404).json('Currency not found');
+            }
+            res.json(element);
+        } else {
+            throw new Error('API request failed');
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
